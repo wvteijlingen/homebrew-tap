640151c5ee07b5e9d960acfeaca0e71c344a372c
Ward van Teijlingen <ward@wardvanteijlingen.nl>
2022-04-09 20:24:56 +0200

    Add localeasy formula

---
+++ Formula/localeasy.rb
@@ -0,0 +1,20 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+class Localeasy < Formula
+  desc "Effortless app localisation using Google Sheets"
+  homepage "https://github.com/wvteijlingen/localeasy"
+  url "https://github.com/wvteijlingen/localeasy/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "23c2a20811892d73dc15075090498de84a4816a3c7bb5a849266832fb120a7e5"
+  license "MIT"
+
+  depends_on "deno" => [:build, :test]
+
+  def install
+    system "deno task compile"
+    bin.install "localeasy"
+  end
+
+  test do
+    system "deno task compile"
+  end
+end
