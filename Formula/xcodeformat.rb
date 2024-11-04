b04a7953426677316cfa87fe5b0b157ade7a26fa
Ward van Teijlingen <1460535+wvteijlingen@users.noreply.github.com>
2024-11-04 12:57:16 +0100

    Update xcodeformat.rb

--- Formula/xcodeformat.rb
+++ Formula/xcodeformat.rb
@@ -5,6 +5,10 @@ class Xcodeformat < Formula
   sha256 "33faffff270b4f1cb2d101a565e386cf1a03fe3966431bf2d09876890af11c69"
   license "MIT"
 
+  def install
+    bin.install "localeasy"
+  end
+
   test do
     system "bin/xcodeformat"
   end
