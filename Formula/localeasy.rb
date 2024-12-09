884538ee6ad97059782d54a186d7033913851c11
Ward van Teijlingen <1460535+wvteijlingen@users.noreply.github.com>
2024-12-09 12:31:40 +0100

    Update localeasy.rb

--- Formula/localeasy.rb
+++ Formula/localeasy.rb
@@ -1,15 +1,16 @@
 class Localeasy < Formula
   desc "Effortless app localisation using Google Sheets"
   homepage "https://github.com/wvteijlingen/localeasy"
-  url "https://github.com/wvteijlingen/localeasy/archive/refs/tags/v0.1.2.tar.gz"
+  url "https://github.com/wvteijlingen/localeasy/archive/refs/tags/v0.2.tar.gz"
   sha256 "23c2a20811892d73dc15075090498de84a4816a3c7bb5a849266832fb120a7e5"
   license "MIT"
 
-  depends_on "deno" => [:build, :test]
+  depends_on xcode: ["16.0", :build]
+  uses_from_macos "swift" => :build
 
   def install
-    system "deno", "task", "compile"
-    bin.install "localeasy"
+    system "swift", "build", *args, "-configuration", "release", "--product", "localeasy"
+    bin.install ".build/release/localeasy"
   end
 
   test do
