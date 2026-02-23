c65c766026db356daf890345e1c795b75c02decc
Ward van Teijlingen <w.van.teijlingen@gmail.com>
2026-02-23 12:40:06 +0100

    Update swift-bsp formula

--- Formula/swift-bsp.rb
+++ Formula/swift-bsp.rb
@@ -2,7 +2,7 @@ class SwiftBsp < Formula
   desc "Build Server Protocol for Swift"
   homepage "https://github.com/wvteijlingen/swift-bsp"
   url "https://github.com/wvteijlingen/swift-bsp/archive/refs/tags/v0.1.tar.gz"
-  sha256 "1201263ccd6acef3c1f49e8e2995ecf4a487fa47e4a4a8b4f4498340e7f4162a"
+  sha256 "2cdd0b002c252063508979f1c69b23335f15c3d3fb09c5d8b9941baca08ae9ed"
   license "MIT"
 
   depends_on xcode: ["26.0", :build]
@@ -12,15 +12,15 @@ class SwiftBsp < Formula
     system "swift", "build", "--disable-sandbox", "--configuration", "release"
 
     bin.install(
-      ".build/release/swift-bsp",
-      ".build/release/SWBBuildServiceBundle",
-      ".build/release/SwiftBuild_SWBAndroidPlatform.bundle",
-      ".build/release/SwiftBuild_SWBApplePlatform.bundle",
-      ".build/release/SwiftBuild_SWBCore.bundle",
-      ".build/release/SwiftBuild_SWBQNXPlatform.bundle",
-      ".build/release/SwiftBuild_SWBUniversalPlatform.bundle",
-      ".build/release/SwiftBuild_SWBWebAssemblyPlatform.bundle",
-      ".build/release/SwiftBuild_SWBWindowsPlatform.bundle"
+      ".build/release/swift-bsp"
+      # ".build/release/SWBBuildServiceBundle",
+      # ".build/release/SwiftBuild_SWBAndroidPlatform.bundle",
+      # ".build/release/SwiftBuild_SWBApplePlatform.bundle",
+      # ".build/release/SwiftBuild_SWBCore.bundle",
+      # ".build/release/SwiftBuild_SWBQNXPlatform.bundle",
+      # ".build/release/SwiftBuild_SWBUniversalPlatform.bundle",
+      # ".build/release/SwiftBuild_SWBWebAssemblyPlatform.bundle",
+      # ".build/release/SwiftBuild_SWBWindowsPlatform.bundle"
     )
   end
 
