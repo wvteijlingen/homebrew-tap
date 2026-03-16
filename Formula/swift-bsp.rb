38948ac7f48d76e114b040fad8e04613a855d8bd
Ward van Teijlingen <w.van.teijlingen@gmail.com>
2026-03-16 14:14:27 +0100

    Update formula for swift-bsp

--- Formula/swift-bsp.rb
+++ Formula/swift-bsp.rb
@@ -4,18 +4,17 @@ class SwiftBsp < Formula
   desc "Build Server Protocol for Swift"
   homepage "https://github.com/wvteijlingen/swift-bsp"
   url "https://github.com/wvteijlingen/swift-bsp/archive/refs/tags/v0.1.tar.gz"
-  sha256 "64a1e7ee288c0fd67c304df1c0c886a91594fad3332fe33d57af878c2b3648ee"
+  sha256 "42e3f8f56ac42341faf7af7f4be3e29f771a9a10613922f8bc068b6b942931ae"
   license "MIT"
 
   depends_on xcode: ["26.0", :build]
   uses_from_macos "swift" => :build
 
   def install
-    puts "Test"
-    system "swift", "build", "--disable-sandbox", "--configuration", "foo"
-    bin.install(".build/release/swift-bsp")
+    system "swift", "build", "--disable-sandbox", "--configuration", "release"
 
     [
+      ".build/release/swift-bsp",
       ".build/release/SWBBuildServiceBundle",
       ".build/release/SwiftBuild_SWBAndroidPlatform.bundle",
       ".build/release/SwiftBuild_SWBApplePlatform.bundle",
@@ -26,10 +25,10 @@ class SwiftBsp < Formula
       ".build/release/SwiftBuild_SWBWebAssemblyPlatform.bundle",
       ".build/release/SwiftBuild_SWBWindowsPlatform.bundle"
     ].each do |source|
-      bin.install(source)
-      # source_name = File.basename(source)
-      # FileUtils.cp_r(source, File.join(bin, source_name))
+      libexec.install(source)
     end
+
+    (bin/"swift-bsp").write_env_script libexec/"swift-bsp", SWBBUILD_SERVICE_BUNDLE: libexec/"SWBBuildServiceBundle"
   end
 
   test do
